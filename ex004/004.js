c5c0f4a3dfcf9b543b54e6c74ab084c4492da391
Kaizen-Maker <68838591+Kaizen-Maker@users.noreply.github.com>
2021-02-11 07:33:47 -0300

    comentário

--- ex004/004.js
+++ ex004/004.js
@@ -1,6 +1,6 @@
 var V = 41
 console.log(`A velocidade do seu carro é ${V} Km/h`)
-if (V > 40) {
+if (V > 40) { //condição simples.
     console.log(`Você ultrapassou a velocidade permitida. Multa!`)
 }
 console.log(`Dirija com cinto de segurança!`)
