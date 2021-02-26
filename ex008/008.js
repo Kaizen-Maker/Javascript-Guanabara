0c5bed9c912a06db711bea9083d97d53f9dd71e3
Kaizen-Maker <68838591+Kaizen-Maker@users.noreply.github.com>
2021-02-26 10:10:25 -0300

    novo exercício

---
+++ ex008/008.js
@@ -0,0 +1,8 @@
+var x = 0.05
+var y = 0.1
+if (x > y){
+    console.log('x é avariável')
+}
+else{
+    console.log('y é a variável')
+}
---
+++ ex009/009.js
@@ -0,0 +1,9 @@
+var estado = 'Pernambuco'
+    console.log(`Você mora em ${estado}`)
+    if(estado == 'Pernambuco'){
+    console.log('Você é pernambucano !')
+}
+    else{
+        console.log('Você é estrangeiro !')
+    }
+
