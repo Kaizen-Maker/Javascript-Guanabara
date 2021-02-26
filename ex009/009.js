615a00b785b4d45bc17260ea0279d70e6b041542
Kaizen-Maker <68838591+Kaizen-Maker@users.noreply.github.com>
2021-02-26 13:35:53 -0300

    Update 009.js

--- ex009/009.js
+++ ex009/009.js
@@ -1,9 +1,9 @@
-var estado = 'Pernambuco'
+var estado = 'Rio grande do sul'
     console.log(`Você mora em ${estado}`)
     if(estado == 'Pernambuco'){
     console.log('Você é pernambucano !')
 }
     else{
-        console.log('Você é estrangeiro !')
+        console.log('Você é sulista !')
     }
 
